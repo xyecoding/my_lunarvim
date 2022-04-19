210d1fc1436136e799d3c6271b3b752a28374cf3
xyegithub <xye@bupt.edu.cn>
2022-04-19 10:13:46 +0800

    new

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -8,7 +8,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
     "clone",
     "--depth",
     "1",
-    "https://github.com/wbthomason/packer.nvim",
+    "git@github.com:wbthomason/packer.nvim.git",
     install_path,
   }
   print "Installing packer close and reopen Neovim..."
@@ -41,58 +41,58 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
   -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-  use "numToStr/Comment.nvim" -- Easily comment stuff
-  use "kyazdani42/nvim-web-devicons"
-  use "kyazdani42/nvim-tree.lua"
-  use "akinsho/bufferline.nvim"
-  use "moll/vim-bbye"
-  use "nvim-lualine/lualine.nvim"
-  use "akinsho/toggleterm.nvim"
-  use "ahmedkhalf/project.nvim"
-  use "lewis6991/impatient.nvim"
-  use "lukas-reineke/indent-blankline.nvim"
-  use "goolord/alpha-nvim"
-  use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
-  use "folke/which-key.nvim"
+  use "git@github.com:wbthomason/packer.nvim.git" -- Have packer manage itself
+  use "git@github.com:nvim-lua/popup.nvim.git" -- An implementation of the Popup API from vim in Neovim
+  use "git@github.com:nvim-lua/plenary.nvim.git" -- Useful lua functions used ny lots of plugins
+  use "git@github.com:windwp/nvim-autopairs.git" -- Autopairs, integrates with both cmp and treesitter
+  use "git@github.com:numToStr/Comment.nvim.git" -- Easily comment stuff
+  use "git@github.com:kyazdani42/nvim-web-devicons.git"
+  use "git@github.com:kyazdani42/nvim-tree.lua.git"
+  use "git@github.com:akinsho/bufferline.nvim.git"
+  use "git@github.com:moll/vim-bbye.git"
+  use "git@github.com:nvim-lualine/lualine.nvim.git"
+  use "git@github.com:akinsho/toggleterm.nvim.git"
+  use "git@github.com:ahmedkhalf/project.nvim.git"
+  use "git@github.com:lewis6991/impatient.nvim.git"
+  use "git@github.com:lukas-reineke/indent-blankline.nvim.git"
+  use "git@github.com:goolord/alpha-nvim.git"
+  use "git@github.com:antoinemadec/FixCursorHold.nvim.git" -- This is needed to fix lsp doc highlight
+  use "git@github.com:folke/which-key.nvim.git"
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
+  use "git@github.com:lunarvim/darkplus.nvim.git"
 
   -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
+  use "git@github.com:hrsh7th/nvim-cmp.git" -- The completion plugin
+  use "git@github.com:hrsh7th/cmp-buffer.git" -- buffer completions
+  use "git@github.com:hrsh7th/cmp-path.git" -- path completions
+  use "git@github.com:hrsh7th/cmp-cmdline.git" -- cmdline completions
+  use "git@github.com:saadparwaiz1/cmp_luasnip.git" -- snippet completions
+  use "git@github.com:hrsh7th/cmp-nvim-lsp.git"
 
   -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  use "git@github.com:L3MON4D3/LuaSnip.git" --snippet engine
+  use "git@github.com:rafamadriz/friendly-snippets.git" -- a bunch of snippets to use
 
   -- LSP
-  use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use "git@github.com:neovim/nvim-lspconfig.git" -- enable LSP
+  use "git@github.com:williamboman/nvim-lsp-installer.git" -- simple to use language server installer
+  use "git@github.com:tamago324/nlsp-settings.nvim.git" -- language server settings defined in json for
+  use "git@github.com:jose-elias-alvarez/null-ls.nvim.git" -- for formatters and linters
 
   -- Telescope
-  use "nvim-telescope/telescope.nvim"
+  use "git@github.com:nvim-telescope/telescope.nvim.git"
 
   -- Treesitter
   use {
-    "nvim-treesitter/nvim-treesitter",
+    "git@github.com:nvim-treesitter/nvim-treesitter.git",
     run = ":TSUpdate",
   }
-  use "JoosepAlviste/nvim-ts-context-commentstring"
+  use "git@github.com:JoosepAlviste/nvim-ts-context-commentstring.git"
 
   -- Git
-  use "lewis6991/gitsigns.nvim"
+  use "git@github.com:lewis6991/gitsigns.nvim.git"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
