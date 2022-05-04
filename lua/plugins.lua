3e100993106f1501a2040b350167abec0cd7db60
xyegithub <xye@bupt.edu.cn>
2022-05-04 16:36:26 +0800

    new

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -59,7 +59,7 @@ lvim.plugins = {
         hsl_fn = true, -- CSS hsl() and hsla() functions
         css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
         css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        lowercase = true,
+        -- lowercase = true,
       })
     end,
   },
