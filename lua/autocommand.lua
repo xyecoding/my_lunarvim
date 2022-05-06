bd23371744cb0b9cd8ab9fc7a8ca1d3428c09c3c
xyegithub <xye@bupt.edu.cn>
2022-05-06 22:26:16 +0800

    new

--- lua/autocommand.lua
+++ lua/autocommand.lua
@@ -1,8 +1,8 @@
 vim.cmd [[
-"  augroup _fold_bug_solution  " https://github.com/nvim-telescope/telescope.nvim/issues/559
-"    autocmd!
-"    autocmd BufRead * autocmd BufWinEnter * ++once normal! zx
-"  augroup end
+  augroup _fold_bug_solution  " https://github.com/nvim-telescope/telescope.nvim/issues/559
+    autocmd!
+    autocmd BufRead * autocmd BufWinEnter * ++once normal! zx
+  augroup end
 
 augroup remember_folds
   autocmd!
